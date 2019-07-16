cf822180e86d9d6cbf67d6451ad30b98fa114030
Marcel Pereira <mcleisson@gmail.com>
2019-07-15 23:32:38 -0300

    modificações URL

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -42,7 +42,7 @@ class UrlsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_url
+    def set_url#
       @urls = Url.find(params[:id])
     end
 
